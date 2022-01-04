25318d3e355373a8ed745737ae686e7def4a62f4
Jeonhakbong <90051532+Jeonhakbong@users.noreply.github.com>
2022-01-04 14:15:50 +0900

    Updated JS 220104

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-const loginForm = document.getElementById("login-form"); // or querySelector('#login-form')
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form"); // or querySelector('#login-form')
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick() {
-  //console.dir(loginInput);
-  console.log(loginInput.value); // we can get the value of the input
-  console.log("click");
+// key word used often
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
+
+function paintGreetings(userName) {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = "Hello, " + userName; // == `Hello &{userName}`
 }
-loginButton.addEventListener("click", onLoginBtnClick);
+
+function onLoginSubmit(event) {
+  const userName = loginInput.value;
+  event.preventDefault(); // previent default behaviors of any events
+
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, userName);
+  paintGreetings(userName);
+}
+
+// variables to check localStorage
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // continue to show
+  paintGreetings(savedUserName);
+}
